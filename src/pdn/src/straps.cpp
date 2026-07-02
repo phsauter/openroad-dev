c73ca9a0cc84b1a5c637113c1eaed772e7b91dda
phsauter <phsauter@student.ethz.ch>
2026-07-02 02:15:51 +0200

    pdn: reduce debug marker noise
    Signed-off-by: phsauter <phsauter@student.ethz.ch>

--- src/pdn/src/straps.cpp
+++ src/pdn/src/straps.cpp
@@ -2670,6 +2670,7 @@ void RepairChannelStraps::repairGridChannels(
         if (marker == nullptr) {
           continue;
         }
+        marker->setTechLayer(channel.connect_to);
         marker->addShape(channel.area);
         for (auto* net : channel.nets) {
           marker->addSource(net);
